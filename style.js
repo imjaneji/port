42177d1fa90423ca77326a0d47ba7a6f47fc9362
imjaneji <98075472+imjaneji@users.noreply.github.com>
2022-01-21 14:48:16 +0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    document.getElementById("cross2").onclick= function(){
+    document.getElementById("next").style.display = "none";
+    };
+    
+    document.getElementById("dropdown").onclick = function(){
+    document.getElementById("next").style.display = "block";
+    };
+    });
